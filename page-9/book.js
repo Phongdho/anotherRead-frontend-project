8546c80a5fee72be8646ad4374ca5f609ab0c644
Phong Ho <phongho@Phongs-MacBook-Pro.local>
2021-10-17 14:52:47 +0700

    adding features to search, and clicking onto each book page

---
+++ page-9/book.js
@@ -0,0 +1,42 @@
+const API_KEY = "H6PdARP2pbEwHjkm6Rrxiw96MaSZ6qIB";
+const baseURL = "https://api.nytimes.com/svc/books/v3";
+let path = "/lists/current/hardcover-fiction.json";
+const query = `?api-key=${API_KEY}`;
+const url = baseURL + path + query;
+
+const cardHTML = document.getElementById("card-group");
+
+//Fetch data 
+const fetchAsync = async () => {
+    let response = await fetch(url);
+    let allData = await response.json();
+    // console.log(typeof allData, allData);
+    let bookData = allData.results.books;
+    let eachBook = bookData[9];
+    console.log(eachBook);
+    function cardOutput () {
+            return `
+            <div class="col">
+            <div class="card-main h-100">
+            <div class="main-left">
+            <h5 class="card-rank">Currently ranked #10 on NYTimes</h5>
+            <img src="${eachBook.book_image}" class="card-img-top" alt="${eachBook.title}"/>
+            </div>
+            <div class="card-body">
+                <h3 class="card-title">${eachBook.title}</h3>
+                <h5 class="card-author">Author: ${eachBook.author}</h5>
+                <p class="card-publisher"><strong>Publisher:</strong> ${eachBook.publisher}</p>
+                <p class="card-id"><strong>ISBN:</strong> ${eachBook.primary_isbn13}</p>
+                <p class="card-text"><strong>Description:</strong> ${eachBook.description}</p>
+                <p class="card-buy"><strong>Get your copy:</strong> <a href="${eachBook.amazon_product_url}">Amazon</a></p>
+            </div>
+            </div>
+            </div>
+            `;
+    };
+    // console.log(cardHTML);
+    cardHTML.innerHTML = cardOutput();
+    };
+
+fetchAsync();
+
--- script.js
+++ script.js
@@ -4,32 +4,58 @@ let path = "/lists/current/hardcover-fiction.json";
 const query = `?api-key=${API_KEY}`;
 const url = baseURL + path + query;
 
-//fetch data 
+const cardHTML = document.getElementById("card-group");
+
+//Fetch data 
 const fetchAsync = async () => {
     let response = await fetch(url);
     let allData = await response.json();
     console.log(typeof allData, allData);
-    const bookData = allData.results.books;
-    const cardHTML = document.getElementById("card-group");
-    const cardOutput = bookData.map((item) => {
-        return renderBook(item);
+    bookData = allData.results.books;
+    const cardOutput = bookData.map((item, index) => {
+        return renderBook(item, index);
     });
     cardHTML.innerHTML = cardOutput.join("");
 }
 fetchAsync();
 
 
-function renderBook (item) {
+function renderBook (item, index) {
     return `
     <div class="col"
         <div class="card-main h-100">
-            <img src="${item.book_image}" class="card-img-top" alt="${item.title}" />
+        <a href="page-${index}/book.html"><img src="${item.book_image}" class="card-img-top" alt="${item.title}" /></a>
             <div class="card-body">
                 <h5 class="card-title">${item.title}</h5>
-                <h6 class="card-author"><em>by</em> ${item.author}</h6>
+                <h6 class="card-author">&#9998; ${item.author}</h6>
                 <p class="card-text">${item.description}</p>
             </div>
         </div>
     </div>
     `;
 };
+
+//Search API-rendered data on the landing page 
+const searchBox = document.getElementById("search-box");
+const searchButton = document.getElementById("search-btn");
+
+const handleSearch = (e) => {
+    e.preventDefault(); //Using server rendering option
+    const titleQuery = searchBox.value;
+    const searchData = bookData.filter((e) => e.title === titleQuery || e.author === titleQuery);
+    const searchValue = searchData.map((item) => {
+        return `
+        <div class="col"
+            <div class="card-main h-100">
+                <img src="${item.book_image}" class="card-img-top" alt="${item.title}" />
+                <div class="card-body">
+                    <h5 class="card-title">${item.title}</h5>
+                    <h6 class="card-author"><em>by</em> ${item.author}</h6>
+                    <p class="card-text">${item.description}</p>
+                </div>
+            </div>
+        </div>`;
+    });
+    cardHTML.innerHTML = searchValue.join("");
+};
+searchButton.addEventListener("click", handleSearch);
